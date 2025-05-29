5d83c89fe796e9213d8634de3a993c254b5493f6
arthurmnz <arthur.gomemenezes@gmail.com>
2025-05-29 10:13:43 -0300

    att admin.py

--- tiss_api/admin.py
+++ tiss_api/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
-from .models import GuiaConsulta, GuiaSPSADT, Procedimento, Prestador, Paciente
+from tiss_api.models.Beneficiario import *
+from tiss_api.models.Procedimento import *
+from tiss_api.models.Contratado import  * 
+from tiss_api.models.Profissional import *
+from tiss_api.models.Guias import *
+from tiss_api.models.Procedimento import *
 
 # Register your models here.
-admin.site.register(Prestador)
-admin.site.register(Paciente)
+admin.site.register(Contratado)
+admin.site.register(Profissional)
+admin.site.register(Beneficiario)
+admin.site.register(Procedimento)
+admin.site.register(ProcedimentoRealizado)
+admin.site.register(ProcedimentosSolicitados)
 admin.site.register(GuiaConsulta)
 admin.site.register(GuiaSPSADT)
-admin.site.register(Procedimento)
