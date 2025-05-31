e89989135901cbfdddc401f185e96e9e6734d695
Enzo dos Anjos <enzodosanjos10@gmail.com>
2025-05-31 20:29:34 -0300

    Mudanças nos campos da classe Contrado

--- tiss_api/models/Contratado.py
+++ tiss_api/models/Contratado.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Contratado(models.Model):  # Campos 9, 10, 11
-    codigo_operadora = models.CharField(max_length=50)  # 9
-    nome_contratado = models.CharField(max_length=255)  # 10
-    cnes = models.CharField(max_length=20)              # 11
+    codigo_operadora = models.CharField(max_length=14, blank=False, null=False)  # 9
+    nome_contratado = models.CharField(max_length=70, blank=False, null=False)  # 10
+    cnes = models.CharField(max_length=7, default='9999999',blank=False, null=False)              # 11
 
     def __str__(self):
         return self.nome_contratado
