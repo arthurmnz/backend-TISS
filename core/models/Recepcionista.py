5214b17556ea1db5a0b5a3a666c68babd71b6a70
Enzo dos Anjos <enzodosanjos10@gmail.com>
2025-07-07 06:25:27 -0300

    Correção na classe Medico e Recepcionista

--- core/models/Recepcionista.py
+++ core/models/Recepcionista.py
@@ -1,18 +1,27 @@
 from django.db import models
+from app.models import Clinica
 
 class Recepcionista(models.Model):
-    nome = models.CharField(max_length=70, blank = False, null = False)
-    cpf = models.CharField(max_length=14, unique=True, blank = False, null = False)
-    email = models.EmailField(unique=True, blank = False, null = False)
-    telefone = models.CharField(max_length=11, blank= False, null = False)
+    GENERO_CHOICES = [
+        ('M', 'Masculino'),
+        ('F', 'Feminino'),
+        ('O', 'Outro'),
+    ]
+
+    nome = models.CharField(max_length=70, blank=False, null=False)
+    genero = models.CharField(max_length=1, choices=GENERO_CHOICES, blank=False, null=False)
+    cpf = models.CharField(max_length=14, unique=True, blank=False, null=False)
+    email = models.EmailField(unique=True, blank=False, null=False)
+    telefone = models.CharField(max_length=11, blank=False, null=False)
     turno = models.CharField(
         max_length=10,
         choices=[('MANHA', 'Manhã'), ('TARDE', 'Tarde'), ('NOITE', 'Noite')],
-        blank = True,
-        null = False
+        blank=True,
+        null=False
     )
-    data_admissao = models.DateField(blank = False, null= False)
-    ativo = models.BooleanField(default=True, blank = False, null = False)
+    data_admissao = models.DateField(blank=False, null=False)
+    ativo = models.BooleanField(default=True, blank=False, null=False)
+    clinica = models.ForeignKey(Clinica, on_delete=models.CASCADE, related_name='recepcionistas', blank=False, null=False)
 
     def __str__(self):
         return self.nome
